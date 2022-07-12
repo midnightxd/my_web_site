e2c6413e923ee471a93c121577e1fd597795762c
midnightxd <leonardigit@gmail.com>
2022-07-12 12:50:14 -0300

    Add navbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import {Container, Box, Heading} from '@chakra-ui/react'
+
+
 const Page = () => {
   return (
-    <div>hello</div>
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={5} align="center">
+        Hello, I&apos;m a full-stack developer!
+      </Box>
+      <Box display={{md: 'flex'}}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            HXVRMXNV
+          </Heading>
+          <p>Digital creator ( Developer )</p>
+        </Box>
+      </Box>
+    </Container>
   )
 }
 
