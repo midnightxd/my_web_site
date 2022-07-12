e41e32279ab0c26cf3f896903ca0698827f097c0
midnightxd <leonardigit@gmail.com>
2022-07-12 16:38:56 -0300

    Add my bio

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "870783903e4720e6"; }
+/******/ 		__webpack_require__.h = function() { return "dbc51e0e1d583a0e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/fonts.js
+++ components/fonts.js
@@ -3,7 +3,7 @@ import { Global } from '@emotion/react';
 const Fonts = () => (
   <Global
     styles={`
-    @import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@200&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300;700&display=swap');
   `}
   />
 );
