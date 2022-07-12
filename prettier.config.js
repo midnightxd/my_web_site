21c473725ff250bd23676f441dae1795e25f2a8e
midnightxd <leonardigit@gmail.com>
2022-07-12 09:37:19 -0300

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tanWidth: 2,
+  traillingComma: 'none',
+}
+
+export default options
